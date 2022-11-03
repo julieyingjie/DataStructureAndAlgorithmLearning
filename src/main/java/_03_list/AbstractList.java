c4b98a9d77a98ce742a688805b478f496f8bd309
Yingjie Jia <julieyingjie@gmail.com>
2022-11-03 00:07:09 -0400

    Add the interface: List, and the Abstract class: AbstractList

---
+++ src/main/java/_03_list/AbstractList.java
@@ -0,0 +1,46 @@
+package _03_list;
+
+//Abstract 的用处：不能实现一个实例
+public abstract class AbstractList<E> implements List<E>{
+    protected int size; // 有效元素的个数
+
+    //Number of elements
+    public int size() {
+        return size;
+    }
+
+    //Is it empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    // Add elements to the end
+    public void add(E element) {
+        add(size, element);
+    }
+
+    //Contains a certain element
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    protected void rangeCheck(int index){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("index: " + index + " , but size is: " + size);
+        }
+    }
+
+    protected void rangeCheckForAdd(int index){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("index: " + index + " , but size is: " + size);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/_03_list/Main.java
@@ -0,0 +1,16 @@
+package _03_list;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> list = new SingleLinkedList<>();
+        list.add(11);
+        list.add(22);
+        list.add(33);
+//        list.add(1,44);
+//        list.add(4,55);
+//        list.remove(2);
+        System.out.println(list.indexOf(33));
+
+
+    }
+}
