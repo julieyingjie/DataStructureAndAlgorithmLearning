1d8ff6a404056de47fc7ece37b28f347dab32769
Yingjie Jia <julieyingjie@gmail.com>
2022-11-02 21:43:24 -0400

    Finish all the API for ArrayList

--- src/main/java/_02_dynamicArray/ArrayList.java
+++ src/main/java/_02_dynamicArray/ArrayList.java
@@ -4,16 +4,25 @@ public class ArrayList<E> {
 
     private int size;
     private E[] elements;
+    /**
+     * Default initial capacity
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+    /**
+     *
+     */
+    private static final int ELEMENT_NOT_FOUND = -1;
+
 
     public ArrayList(){
-
+        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
-    public ArrayList(int capacity){
-
+    public ArrayList(int capacity){ // capacity 修饰的是容器的大小。但容器不一定会装满
+        capacity = capacity < DEFAULT_CAPACITY ? DEFAULT_CAPACITY: capacity;
+        this.elements = (E[]) new Object[capacity];
     }
-
-
 
     //Number of elements
     public int size() {
@@ -30,23 +39,17 @@ public class ArrayList<E> {
         size = 0;
     }
 
-    //Contains a certain element
-    public boolean contains(E element) {
-        return false;
-    }
-
     // Add elements to the end
     public void add(E element) {
         add(size, element);
-
     }
 
     // Add elements to the index position
     public void add(int index, E element) {
         // check the input index is valid or not
-        if (index < 0 || index > size){
-            throw new IndexOutOfBoundsException("index: " + index + " , but size is: " + size);
-        }
+        rangeCheckForAdd(index);
+        // ensure the capacity
+        ensureCapacity(size + 1);
 
         //head, current position
         for (int i = size; i > index; i--) {
@@ -62,23 +65,78 @@ public class ArrayList<E> {
     // Returns the element corresponding to the index position
     public E get(int index) {
         // Check the input index is valid or not
+        rangeCheck(index);
         return elements[index];
     }
 
+    //Contains a certain element
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+
     // Set the element at the index position
     public E set(int index, E element) {
-        return null;
+        rangeCheckForAdd(index);
+
+        E oldElement = elements[index];
+        elements[index] = element;
+
+        return oldElement;
     }
 
 
     // Delete elements to the index position
     public E remove(int index) {
-        return null;
+        // check index
+        rangeCheck(index);
+
+        // remove, head, current
+        E oldElement = elements[index];
+        for (int i = index + 1; i < size ; i++) {
+            elements[i - 1] = elements[i];
+        }
+        elements[--size] = null;
+
+        return oldElement;
     }
 
     // Return the index of the element
-    public int indexOf(E element) {
-        return 0;
+    public int indexOf(E element) { // element 有可能 NULL
+        if (element == null){
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null){ return i;}
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (element.equals(elements[i])){ return i;}
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    private void ensureCapacity(int capacity){
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) return;
+        //扩容, 每次1。5倍。
+        int newCapacity = oldCapacity + oldCapacity >> 1; // 这种写法，可以保证精度, 专门用于小数转整数时的丢失
+        // 值的迁移
+        E[] newElement = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElement[i]  = elements[i];
+        }
+        elements = newElement;
+    }
+
+    private void rangeCheckForAdd(int index){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("index: " + index + " , but size is: " + size);
+        }
+    }
+
+    private void rangeCheck(int index){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("index: " + index + " , but size is: " + size);
+        }
     }
 
 
--- src/main/java/_02_dynamicArray/Main.java
+++ src/main/java/_02_dynamicArray/Main.java
@@ -1,8 +1,29 @@
 package _02_dynamicArray;
 
-
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(10 + i);
+        }
+        list.remove(4);
+        list.add(null);
+        int index = list.indexOf(null);
+        System.out.println(index);
+        System.out.println(list.contains(10));
+        System.out.println(list.contains(null));
+        System.out.println(list.set(1,100));
+//        System.out.println(list.size());
+//        System.out.println(list.size());
+//        list.add(100);
+//        list.add(1, 10);
+//        list.add(0, 30);
+//        list.add(1,20);
+////        list.add(5, 10); this is for test the exception
+//        System.out.println(list.get(3));
+//        System.out.println(list.size());
+//        list.clear();
+//        System.out.println(list.isEmpty());
 
     }
 }
