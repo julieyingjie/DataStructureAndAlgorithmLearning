593661699585bbd881d191b3799167110a6d720d
Yingjie Jia <julieyingjie@gmail.com>
2022-11-21 23:18:51 -0500

    add a solution using node to solve the minstack problem

--- src/main/java/_00_leetcode/_02_stack/MinStack.java
+++ src/main/java/_00_leetcode/_02_stack/MinStack.java
@@ -1,5 +1,6 @@
 package _00_leetcode._02_stack;
 
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -7,44 +8,88 @@ import java.util.Stack;
  * https://leetcode.com/problems/min-stack/
  * 考察如何对流动数据进行记录，操作
  * 不要遍历
- * 两个stack 容器 data 和 min
+ * idea1: 两个stack 容器 data 和 min
+ * idea2: 利用node 的思想。node 当中，有val, next 指针, min 指针
  */
 
 public class MinStack {
-        private Stack<Integer> dataStack;
-        private Stack<Integer> minStack;
+
+        // idea1: 两个stack 容器 data 和 min
+//        private Stack<Integer> dataStack;
+//        private Stack<Integer> minStack;
+//
+//        public MinStack() {
+//                dataStack = new Stack<>();
+//                minStack = new Stack<>();
+//        }
+//
+//        public void push(int val) {
+//                dataStack.push(val);
+//
+//                if (minStack.isEmpty())  minStack.push(val);
+//                else if (val <= minStack.peek()){ // 注意这里，有个=号
+//                        minStack.push(val);
+//                }
+//        }
+//
+//        public void pop() {
+//                int pop = 0;
+//                if (!dataStack.isEmpty()) {
+//                        pop = dataStack.pop();
+//                }
+//
+//                if (pop == minStack.peek()) minStack.pop();
+//
+//        }
+//
+//        public int top() {
+//            return dataStack.peek();
+//        }
+//
+//        public int getMin() {
+//                if (minStack.isEmpty()) throw new RuntimeException("Stack is empty");
+//            return minStack.peek();
+//        }
+
+
+        // idea 2: 利用node 的思想。node 当中，有val, next 指针, min 指针
+
+        // 1. 创造这么一个node 作为inner class
+        private static class Node{
+                int value;
+                Node next;
+                int min;
+
+                public Node(int value, Node next, int min) {
+                        this.value = value;
+                        this.next = next;
+                        this.min = min;
+                }
+        }
+
+        Node head;
 
         public MinStack() {
-                dataStack = new Stack<>();
-                minStack = new Stack<>();
+                // dummy node
+                head = new Node(0,null,Integer.MAX_VALUE);
         }
 
         public void push(int val) {
-                dataStack.push(val);
-
-                if (minStack.isEmpty())  minStack.push(val);
-                else if (val <= minStack.peek()){ // 注意这里，有个=号
-                        minStack.push(val);
-                }
+               head = new Node(val, head, Math.min(val, head.min));
         }
 
         public void pop() {
-                int pop = 0;
-                if (!dataStack.isEmpty()) {
-                        pop = dataStack.pop();
-                }
-
-                if (pop == minStack.peek()) minStack.pop();
-
+                head = head.next;
         }
 
         public int top() {
-            return dataStack.peek();
+
+                return head.value;
         }
 
         public int getMin() {
-                if (minStack.isEmpty()) throw new RuntimeException("Stack is empty");
-            return minStack.peek();
+
+                return head.min;
         }
 
 
