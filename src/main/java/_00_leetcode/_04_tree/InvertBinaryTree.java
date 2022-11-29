5bfd3f5d202cab281e39dd9fe80ef50abd5d870a
Yingjie Jia <julieyingjie@gmail.com>
2022-11-29 13:20:17 -0500

    using the 4 traversal ways to InvertBinaryTree

--- src/main/java/_00_leetcode/_04_tree/InvertBinaryTree.java
+++ src/main/java/_00_leetcode/_04_tree/InvertBinaryTree.java
@@ -11,6 +11,9 @@ import java.util.Queue;
  */
 
 public class InvertBinaryTree {
+
+
+    // This is the way using preOrderTraversal
     public TreeNode invertTree1(TreeNode root) {
 
         //base case;
@@ -26,18 +29,54 @@ public class InvertBinaryTree {
         return root;
     }
 
+    // this is the way using postOrderTraversal
+    public TreeNode invertTree2(TreeNode root) {
+
+        if (root == null) return null;
+
+        invertTree2(root.left);
+        invertTree2(root.right);
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        return root;
+    }
+
+
+    // This is the way using levelTraversal
     public TreeNode invertTree3(TreeNode root){
         if (root == null) return null;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+
         while (!queue.isEmpty()){
             TreeNode poll = queue.poll();
+
             TreeNode temp = poll.left;
             poll.left = poll.right;
             poll.right = temp;
+
             if (poll.left != null) queue.offer(poll.left);
             if (poll.right != null) queue.offer(poll.right);
         }
         return root;
+    }
+
+    // This is the way using inorder Traversal
+    public TreeNode invertTree4(TreeNode root) {
+        if (root == null) return null;
+
+        invertTree4(root.left);
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree4(root.left);
+
+        return root;
+
     }
 }
